21c03dd3bfb0cc97f82753b0d6a7328ea42a3456
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-09-16 10:23:30 +1200

    apply palette swap to directories as well as files, and improve error handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,24 @@
-use std::{env, fs};
+use std::{env, fs, io};
 use std::collections::HashMap;
+use std::ffi::OsStr;
+use std::path::Path;
 
 use ase;
-use ase::{Cel, CelChunk, ChunkData, ColorDepth, RGBA256, Pixels, Aseprite};
-use ase::Cel::{RawCel, CompressedImage};
-use flate2::Compression;
-use flate2::write::ZlibEncoder;
+use ase::{Aseprite, Cel, CelChunk, ChunkData, ColorDepth, Pixels, RGBA256};
+use ase::Cel::{CompressedImage, RawCel};
+use log::{error, info, warn};
 use simple_logger::SimpleLogger;
-
-use log::{info, warn};
-use std::path::Path;
+use walkdir::WalkDir;
 
 // TODO maybe we want a way to ignore alpha?
 type PaletteMap = HashMap<RGBA256, RGBA256>;
+
+fn is_ase_extension(ext: Option<&OsStr>) -> bool {
+	if let Some(ext) = ext {
+		return ext == "aseprite" || ext == "ase"
+	}
+	false
+}
 
 fn transform_pixels<F>(chunk: &mut CelChunk, func: F)
 		where F: FnOnce(&mut Pixels) {
@@ -29,7 +35,7 @@ fn transform_pixels<F>(chunk: &mut CelChunk, func: F)
 					*zlib_compressed_data = data;
 				},
 				Err(e) => {
-					warn!("Error during zlib compression, skipping cel:\n{:?}", e);
+					warn!("Error during zlib compression, skipping cel:\n{}", e);
 				}
 			}
 		},
@@ -45,11 +51,10 @@ fn palette_swap_ase(ase: &mut Aseprite, palette: &PaletteMap) {
 			use ChunkData::*;
 			match data {
 				OldPaletteChunk4(_) | OldPaletteChunk11(_) => {
-					warn!("Found deprecated palette chunk, skipping");
+					// warn!("Found deprecated palette chunk, skipping");
 				},
 				PaletteChunk(data) => {
-					println!("Found palette chunk");
-					println!("{:?}", data);
+					// TODO handle palette data updating
 				},
 				CelChunk(data) => {
 					transform_pixels(data, |p| {
@@ -111,30 +116,71 @@ fn ase_to_palettemap(ase: &Aseprite) -> PaletteMap {
 	palette
 }
 
+fn load_palette(path: &Path) -> Result<PaletteMap, io::Error> {
+	let mut file = fs::File::open(path)?;
+	let ase = Aseprite::from_read(&mut file)?;
+	Ok(ase_to_palettemap(&ase))
+}
+
+fn palette_swap_path(palette: &PaletteMap, input: &Path, output: &Path) -> Result<(), io::Error> {
+	let mut ase = fs::File::open(input)?;
+	let mut ase = ase::Aseprite::from_read(&mut ase)?;
+	palette_swap_ase(&mut ase, &palette);
+	let mut file = fs::OpenOptions::new().create_new(true).write(true).open(output)?;
+	ase.write(&mut file)?;
+	Ok(())
+}
+
 fn main() {
 	SimpleLogger::new().init().unwrap_or_else(|e| eprintln!("Failed to create logger: {}", e));
 
 	// TODO will probably want to switch to an actual argument parsing library soon
 	let mut args = env::args().skip(1);
 	let palette_path = args.next().expect("Missing first argument (palette file)");
+	let palette_path = Path::new(&palette_path);
 	let input_path = args.next().expect("Missing second argument (input file or directory)");
 	let input_path = Path::new(&input_path);
 	let output_path = args.next().expect("Missing third argument (output file or directory)");
+	let output_path = Path::new(&output_path);
+	if output_path.exists() {
+		error!("Output file/directory already exists, please remove it first.");
+		return;
+	}
 
-	let palette = {
-		let mut file = fs::File::open(palette_path).expect("Failed to load palette file");
-		let ase = Aseprite::from_read(&mut file).expect("Failed to load palette file");
-		ase_to_palettemap(&ase)
+	let palette = match load_palette(&palette_path) {
+		Ok(palette) => palette,
+		Err(e) => {
+			error!("Failed to load palette swap image:\n{}", e);
+			return;
+		}
 	};
 
 	if input_path.is_dir() {
-		// TODO
+		for entry in WalkDir::new(input_path).into_iter()
+				.filter_map(|e| e.ok()).filter(|e| is_ase_extension(e.path().extension())) {
+			let path = entry.path();
+			// TODO shouldn't use this expect here
+			let output_path = output_path.join(path.strip_prefix(input_path).expect("Failed to strip path prefix"));
+
+			let output_folder = output_path.parent();
+			if let Some(output_folder) = output_folder {
+				info!("Making output folder {}", output_folder.display());
+				if let Err(e) = fs::create_dir_all(output_folder) {
+					warn!("Error making output directory {}:\n{}", output_folder.display(), e);
+				}
+			}
+
+			if let Err(e) = palette_swap_path(&palette, &path, &output_path) {
+				warn!("Error processing file {}:\n{}", input_path.display(), e);
+			} else {
+				info!("Outputted file {}", output_path.display());
+			}
+		}
 	} else {
-		let mut ase = fs::File::open(input_path).expect("Failed to load input file or directory");
-		let mut ase = ase::Aseprite::from_read(&mut ase).expect("Failed to load input file");
-		palette_swap_ase(&mut ase, &palette);
-		// TODO prevent overwriting
-		let mut file = fs::File::create(output_path).unwrap();
-		ase.write(&mut file);
+		if let Err(e) = palette_swap_path(&palette, &input_path, &output_path) {
+			warn!("Error processing file {}:\n{}", input_path.display(), e);
+		} else {
+			info!("Outputted file {}", output_path.display());
+		}
 	}
 }
