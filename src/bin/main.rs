2226965385b7983496808220a15340c9593fdd09
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-09-15 18:55:35 +1200

    in-place pixel replacement

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,8 +2,10 @@ use std::{env, fs};
 use std::collections::HashMap;
 
 use ase;
-use ase::{Cel, ChunkData, ColorDepth, RGBA256, CelChunk};
-use ase::Cel::RawCel;
+use ase::{Cel, CelChunk, ChunkData, ColorDepth, RGBA256, Pixels};
+use ase::Cel::{RawCel, CompressedImage};
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
 use simple_logger::SimpleLogger;
 
 use log::{info, warn};
@@ -11,36 +13,45 @@ use log::{info, warn};
 // TODO maybe we want a way to ignore alpha?
 type PaletteMap = HashMap<RGBA256, RGBA256>;
 
-/// Apply a palette swap to the pixels in a cel
-/// Requires that the cel is in RGBA format, otherwise everything will probably break horribly.
-/// If the cel is a LinkedCel, has no effect.
-fn palette_swap_cel(/*palette: &PaletteMap, */chunk: &mut CelChunk) {
+fn transform_pixels<F>(chunk: &mut CelChunk, func: F)
+		where F: FnOnce(&mut Pixels) {
 	let cel = &mut chunk.cel;
-	// TODO can't mutate Cel's pixels in place; we'll have to return a new Cel
-	use Cel::*;
+	println!("{:?}", cel);
 	match cel {
-		RawCel {..} | CompressedImage {..} => {
-			match cel.pixels(ColorDepth.RGBA) {
-				None => {
-					// TODO actually useful warn message
-					warn!("Failed to get pixels from cel");
+		RawCel { ref mut pixels, .. } => {
+			func(pixels);
+		},
+		CompressedImage { ref mut zlib_compressed_data, .. } => {
+			let mut pixels = Cel::decompress_pixels(&zlib_compressed_data, &ColorDepth::RGBA);
+			func(&mut pixels);
+			let v = Vec::new();
+			let mut z = ZlibEncoder::new(v, Compression::default());
+			pixels.write(&mut z);
+			match z.finish() {
+				Ok(data) => {
+					*zlib_compressed_data = data;
 				},
-				Some(pixels) => {
-
+				Err(e) => {
+					warn!("Error during zlib compression, skipping cel:\n{:?}", e);
 				}
 			}
 		},
+		// Ignore linked cels
 		_ => {}
-	}
+	};
+	println!("{:?}", cel);
 }
 
 fn main() {
 	SimpleLogger::new().init().unwrap_or_else(|e| eprintln!("Failed to create logger: {}", e));
 
-	let file_name = env::args().skip(1).next().unwrap();
+	let mut args = env::args().skip(1);
+	let file_name = args.next().unwrap();
+	let out_name = args.next().unwrap();
 	let mut file = fs::File::open(file_name).unwrap();
 	let mut ase = ase::Aseprite::from_read(&mut file).unwrap();
 	drop(file);
+
 	println!("{:#?}", ase.header);
 	for frame in ase.frames.iter_mut() {
 		for chunk in frame.chunks.iter_mut() {
@@ -61,10 +72,24 @@ fn main() {
 					println!("Found cel chunk");
 					// let cel = &mut data.cel;
 					// println!("{:?}", cel);
-					palette_swap_cel(data);
+					// palette_swap_cel(data);
+					transform_pixels(data, |p| {
+						match p {
+							Pixels::RGBA(p) => {
+								for pixel in p.iter_mut() {
+									pixel.r /= 2;
+									pixel.g /= 2;
+									pixel.b /= 2;
+								}
+							}
+							_ => {}
+						}
+					});
 				}
 				_ => {}
 			}
 		}
 	}
+	let mut file = fs::File::create(out_name).unwrap();
+	ase.write(&mut file);
 }
